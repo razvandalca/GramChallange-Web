4174165b023279fce492683079681150b6cbc688
Razvan Dalca <dalca.razvan@gmail.com>
2019-09-27 15:23:37 +0300

    Ignore - Remove Failing unit test

--- GramChallange/gram_challange_app/unit_tests/test_trial.py
+++ GramChallange/gram_challange_app/unit_tests/test_trial.py
@@ -5,8 +5,5 @@ class TrialTest(unittest.TestCase):
     def test_passes(self):
         self.assertEqual(self.TEST_NAME, "HEY")
 
-    def test_fail(self):
-        self.assertEqual(self.TEST_NAME, "HY")
-
     def setUp(self):
         self.TEST_NAME = "HEY"
