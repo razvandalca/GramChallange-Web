99e545d1efad44b7e7310831eb506dcaea9f1cb9
Razvan Dalca <dalca.razvan@gmail.com>
2019-10-18 16:22:42 +0300

    [UPDATE] Update folder names and structure and fix misspelled name

--- GramChallange/gram_challange_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GramChallangeAppConfig(AppConfig):
-    name = 'GramChallange.gram_challange_app'
--- GramChallenge/urls.py
+++ GramChallenge/urls.py
@@ -1,4 +1,4 @@
-"""GramChallange URL Configuration
+"""GramChallenge URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
